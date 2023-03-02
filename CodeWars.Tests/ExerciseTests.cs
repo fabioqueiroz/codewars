017a9a66ad61814630eae194d20f14c2fdbe5b01
Fabio Queiroz <fabio.queiroz@isometrix.com>
2023-03-02 16:40:04 +0000

    string exercises

---
+++ CodeWars.Tests/ExerciseTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using FluentAssertions;
+using System.Runtime.CompilerServices;
+
+namespace CodeWars.Tests
+{
+    public class ExerciseTests
+    {
+        [Test]
+        [TestCase("ABCD", "^", "A^B^C^D")]
+        [TestCase("chocolate", "-", "c-h-o-c-o-l-a-t-e")]
+        public void WhenCalled_ShouldAddSeparatorSuccessfully(string input, string separator, string output)
+        {
+            // Arrange
+            // Act
+            var result = Exercises.AddSeparator(input, separator);
+
+            // Assert
+            result.Should().Be(output);
+        }
+
+        [Test]
+        [TestCase("qwerty", "ytrewq")]
+        [TestCase("oe93 kr", "rk 39eo")]
+        public void WhenCalled_ShouldReverseOrderSuccessfully(string input, string output)
+        {
+            // Arrange
+            // Act
+            var result = Exercises.ReverseOrder(input);
+
+            // Assert
+            result.Should().Be(output);
+        }
+
+        [Test]
+        [TestCase("1q2w3e", 6)]
+        [TestCase("L0r3m.1p5um", 9)]
+        [TestCase("", 0)]
+        public void WhenCalled_ShouldFindSumOfDigitsSuccessfully(string input, int output)
+        {
+            // Arrange
+            // Act
+            var result = Exercises.FindSumOfDigits(input);
+
+            // Assert
+            result.Should().Be(output);
+        }
+    }
+}
---
+++ CodeWars/Exercises.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class Exercises
+    {
+        // Given a string and a separator, write a method that adds separator between each adjacent characters in a string.
+        // ex: AddSeparator("ABCD", "^") → "A^B^C^D"
+        public static string AddSeparator(string input, string separator)
+        {
+            return string.Join(separator, input.ToCharArray());
+        }
+
+        // Given a string, write a method that returns that string in reverse order.
+        public static string ReverseOrder(string input)
+        {
+            //var result = new StringBuilder();
+            //for (int i = input.Length - 1; i >= 0; i--)
+            //{
+            //    result.Append(input[i]);
+            //}
+            //return result.ToString();
+
+            return string.Concat(input.Reverse());
+        }
+
+        // Given a string, write a method which returns sum of all digits in that string. Assume that string contains only single digits.
+        public static int FindSumOfDigits(string input)
+        {
+            return input.Where(c => char.IsDigit(c)).Select(c => (int)char.GetNumericValue(c)).Sum();
+        }
+    }
+}
