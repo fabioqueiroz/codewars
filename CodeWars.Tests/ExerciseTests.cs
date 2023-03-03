cbda5391f951bad36d39997880a6f116d6e62eb9
Fabio Queiroz <fabio.queiroz@isometrix.com>
2023-03-03 16:03:27 +0000

    string methods

--- CodeWars.Tests/ExerciseTests.cs
+++ CodeWars.Tests/ExerciseTests.cs
@@ -64,5 +64,86 @@ namespace CodeWars.Tests
             // Assert
             result.Should().Be(output);
         }
+
+        [Test]
+        [TestCase("aaa", "BBB", "aBaBaB")]
+        [TestCase("good one", "111", "g1o1o1d one")]
+        [TestCase("DoG", "ElEpHaNt", "DEolGEpHaNt")]
+        public void WhenCalled_ShouldMixTwoStrings(string leftInput, string rightInput, string output)
+        {
+            // Arrange
+            // Act
+            var result = Exercises.MixTwoStrings(leftInput, rightInput);
+
+            // Assert
+            result.Should().Be(output);
+        }
+
+        [Test]
+        [TestCase("This is sample sentence", 4)]
+        [TestCase("OK", 1)]
+        public void WhenCalled_ShouldReturnNumberOfWords(string input, int output)
+        {
+            // Arrange
+            // Act
+            var result = Exercises.NumberOfWords(input);
+
+            // Assert
+            result.Should().Be(output);
+        }
+
+        [Test]
+        [TestCase("John Doe.", "Doe. John")]
+        [TestCase("A, B. C", "C B. A,")]
+        public void WhenCalled_ShouldRevertWordsOrder(string input, string output)
+        {
+            // Arrange
+            // Act
+            var result = Exercises.RevertWordsOrder(input);
+
+            // Assert
+            result.Should().Be(output);
+        }
+
+        [Test]
+        [TestCase("do it now", "do", 1)]
+        [TestCase("empty", "d", 0)]
+        public void WhenCalled_ShouldReturnHowManyOccurrences(string leftInput, string rightInput, int output)
+        {
+            // Arrange
+            // Act
+            var result = Exercises.HowManyOccurrences(leftInput, rightInput);
+
+            // Assert
+            result.Should().Be(output);
+        }
+
+        [Test]
+        [TestCase("onomatopoeia", "tpoooonmieaa")]
+        [TestCase("fohjwf42os", "wsoojhff42")]
+        [TestCase("Thi2 12  5@mpl3 Str!nG~", "TtSrpnmlihG53221~@!")]
+        //[TestCase("Thi2 12  5@mpl3 Str!nG~", "~trpnmlihTSG@53221!")]
+        public void WhenCalled_ShouldSortCharactersDescending(string input, string output)
+        {
+            // Arrange
+            // Act
+            var result = Exercises.SortCharactersDescending(input);
+
+            // Assert
+            result.Should().Be(output);
+        }
+
+        [Test]
+        [TestCase("kkkktttrrrrrrrrrr", "k4t3r10")]
+        [TestCase("p555ppp7www", "p153p371w3")]
+        public void WhenCalled_ShouldCompressString(string input, string output)
+        {
+            // Arrange
+            // Act
+            var result = Exercises.CompressString(input);
+
+            // Assert
+            result.Should().Be(output);
+        }
     }
 }
--- CodeWars/Exercises.cs
+++ CodeWars/Exercises.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CodeWars
@@ -46,7 +48,116 @@ namespace CodeWars
             //return result.ToString();
 
             return string.Concat(input.Select((c, index) => index % 2 == 0 ? char.ToUpper(c) : c));
+        }
 
+        // Given two strings, write a method that returns one string made of two strings. First letter of new string is first letter of first string,
+        // second letter of new string is first letter of second string and so on.
+        public static string MixTwoStrings(string leftInput, string rightInput)
+        {
+            var result = new StringBuilder();
+
+            for (int i = 0; i < (leftInput.Length >= rightInput.Length ? leftInput.Length : rightInput.Length); i++)
+            {
+                if (i < leftInput.Length)
+                {
+                    result.Append(leftInput[i]);
+                }
+                if (i < rightInput.Length)
+                {
+                    result.Append(rightInput[i]);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        // Given a string, write a method that counts its number of words. Assume there are no leading and trailing whitespaces and
+        // there is only single whitespace between two consecutive words.
+        public static int NumberOfWords(string input)
+        {
+            //var numberOfSpaces = 0;
+
+            //for (int i = 0; i < input.Trim().Length; i++)
+            //{
+            //    if (char.IsWhiteSpace(input[i]))
+            //    {
+            //        numberOfSpaces++;
+            //    }
+            //}
+
+            //return numberOfSpaces + 1;
+
+            return Regex.Matches(input, @"[a-zA-Z\b]+").Count;
+        }
+
+        // Given a string, write a method that returns new string with reverted words order. Pay attention to the punctuation at the end of the sentence (period).
+        public static string RevertWordsOrder(string input)
+        {
+            //var regex = new Regex("[a-zA-Z\\S\\b]+");
+            //var words = regex.Matches(input).ToArray();
+            //var result = string.Empty;
+
+            //for (int i = words.Length - 1; i >= 0; i--)
+            //{
+            //    result += $" {words[i]}";
+            //}
+
+            //return result;
+
+            return string.Join(" ", input.Split(" ").Reverse());
+        }
+
+        // Given a string and substring, write a method that returns number of occurrences of substring in the string.
+        // Assume that both are case-sensitive. You may need to use library function here.
+        public static int HowManyOccurrences(string input, string inputToSearch)
+        {
+            //var splittedWords = input.Split(" ");
+            //var counter = 0;
+            //for (int i = 0; i < splittedWords.Length; i++)
+            //{
+            //    if (splittedWords[i].Contains(inputToSearch))
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //return counter;
+
+            var words = Regex.Matches(input, @"[a-zA-Z\b]+").ToList();
+            var result = words.Where(w => w.ToString().Contains(inputToSearch)).ToList();
+            return result.Count;
+        }
+
+        // Given a string, write a method that returns string/array of chars (ASCII characters) sorted in descending order.
+        public static string SortCharactersDescending(string input)
+        {
+            var result = string.Concat(input.ToArray().Where(c => char.IsAscii(c)).OrderByDescending(c => c.ToString()));
+            return result.Trim();
+        }
+
+        // Given a non-empty string, write a method that returns it in compressed format.
+        // ex: CompressString("kkkktttrrrrrrrrrr") → "k4t3r10", CompressString("p555ppp7www") → "p153p371w3"
+        public static string CompressString(string input)
+        {
+            var counter = 0;
+            var letter = input[0];
+            var result = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (letter == input[i])
+                {
+                    counter++;
+                }
+                else
+                {
+                    result += ($"{letter}{counter}");
+                    letter = input[i];
+                    counter = 1; 
+                }                
+            }
+
+            result += $"{letter}{counter}";
+            return result;
         }
     }
 }
